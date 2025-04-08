3ff6669e1c94dca5d7869bdd97f85e8dd0f3d8d1
HHV2610 <161548459+HHV2610@users.noreply.github.com>
2025-04-08 16:42:48 +1000

    Update todo.py

--- .github/scripts/todo.py
+++ .github/scripts/todo.py
@@ -1,1 +1,15 @@
+class Task:
+    def __init__(self, title, status="ToDo"):
+        self.title = title
+        self.completed = False
+        self.status = status
 
+    def mark_completed(self):
+        self.completed = True
+        self.status = "Done"
+
+    def __repr__(self):
+        return f"{self.title} - {'Done' if self.completed else 'ToDo'}"
+
+    def __str__(self):
+        return f"Task: {self.title}, Status: {self.status}"
